3b116e16a31a3246e2aaa401ae5f23f52dc5353a
apawate <apawate739@student.fuhsd.org>
2021-05-17 18:28:45 -0700

    Configuring library for PyPI.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graphscii",
+    version="0.0.1",
+    author="Agastya Pawate",
+    author_email="apawate739@student.fuhsd.org",
+    description="A Python library for making graphics using ASCII characters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apawate/graphscii",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GPL V3",
+        "Operating System :: OS Independent",
+    ],
+)

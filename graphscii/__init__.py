879228ed1d23689b783f0afc28360e4484b978f9
apawate <apawate739@student.fuhsd.org>
2021-05-17 19:22:54 -0700

    Renamed to graphsciics.

--- graphscii/__init__.py
+++
@@ -1,1 +0,0 @@
-from graphscii import *
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="graphscii",
+    name="graphsciics",
     version="0.0.1",
     author="Agastya Pawate",
     author_email="apawate739@student.fuhsd.org",
     description="A Python library for making graphics using ASCII characters.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/apawate/graphscii",
+    url="https://github.com/apawate/graphsciics",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

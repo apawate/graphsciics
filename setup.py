fd5bb973e305ba5ba7a541a3820d91dabcac18a7
apawate <apawate739@student.fuhsd.org>
2021-05-17 18:54:34 -0700

    Added stuff to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: GNU GPL V3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )

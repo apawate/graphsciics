054245711f1417e2bba0c0fca9c644ded78b769b
apawate739 <apawate739@LHS-LAB608-30.local>
2021-05-18 10:28:24 -0700

    Added animation support.

---
+++ graphsciics/animate.py
@@ -0,0 +1,15 @@
+from graphsciics import *
+import os
+import time
+
+class Animation:
+	def __init__(self, delay):
+		self.frames = []
+		self.delay = delay
+	def addframe(self, obj):
+		self.frames.append(obj)
+	def display(self):
+		for shape in self.frames:
+			shape.display()
+			time.sleep(self.delay)
+			os.system("clear")
